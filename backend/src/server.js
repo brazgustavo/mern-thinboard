2b717d2d11148ca46b6e52991e85f7fc498c82c9
Gustavo Braz <brazgustavo@johndeere.com>
2026-09-09 18:19:12 -0300

    tes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,42 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path"
 
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 
 
 //middleware
-app.use(cors());
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }
+        
+    ));
+}
+
+
+
 app.use(express.json()); //this middleware will parse JSON bodies
 app.use(rateLimiter);
 
 
 
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res) =>{
+    res.semdFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 connectDB().then(() => {
     app.listen(PORT, ()=> {
